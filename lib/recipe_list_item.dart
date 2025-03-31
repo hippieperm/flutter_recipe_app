a11fc64111a26d3e727f4f6f1e24ba24a3eb2a92
hippieperm <leess0133@gmail.com>
2025-03-31 14:13:08 +0900

    레시피 리스트 아이템 및 메뉴 위젯 수정: RecipeListItem과 RecipeMenu 클래스에서 레시피 제목을 각각 'RecipeListItem'과 'RecipeMenu'로 변경함

--- lib/recipe_list_item.dart
+++ lib/recipe_list_item.dart
@@ -5,6 +5,6 @@ class RecipeListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text('RecipeTitle');
+    return Text('RecipeListItem');
   }
 }
--- lib/recipe_menu.dart
+++ lib/recipe_menu.dart
@@ -5,6 +5,6 @@ class RecipeMenu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text('RecipeTitle');
+    return Text('RecipeMenu');
   }
 }
