3e55c2b47865a0c36bc72121186115d7e3431b84
hippieperm <leess0133@gmail.com>
2025-03-31 14:41:22 +0900

    레시피 제목 위젯 수정: RecipeTitle 클래스의 텍스트를 'RecipeTitle'에서 'Recipes'로 변경하고, 위젯에 패딩을 추가하여 레이아웃을 개선함

--- lib/recipe_title.dart
+++ lib/recipe_title.dart
@@ -5,6 +5,14 @@ class RecipeTitle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text('RecipeTitle');
+    return Padding(
+      padding: EdgeInsets.only(top: 20),
+      child: Text(
+        'Recipes',
+        style: TextStyle(
+          fontSize: 30,
+        ),
+      ),
+    );
   }
 }
